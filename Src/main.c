a98b0a70a95f4150be45a5f47a7fe21869ada2e7
trionking <trionking1@gmail.com>
2018-09-02 14:57:06 +0900

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -165,7 +165,7 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = 4;
-  RCC_OscInitStruct.PLL.PLLN = 164;
+  RCC_OscInitStruct.PLL.PLLN = 162;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 8;
   RCC_OscInitStruct.PLL.PLLR = 2;
